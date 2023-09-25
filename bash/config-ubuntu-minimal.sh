a2da892cc8271a381cbf5a41d404b26cdf6de2cc
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-09-25 15:18:01 -0600

    feat: :sparkles: change environment to vanila gnome

--- bash/config-ubuntu-minimal.sh
+++ bash/config-ubuntu-minimal.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+# Cambia la configuración de needrestart para reiniciar automáticamente los servicios
+sudo sed -i 's/#$nrconf{restart} = .*/$nrconf{restart} = "a";/' /etc/needrestart/needrestart.conf
+
+# Muestra el contenido actual del archivo para verificar el cambio
+cat /etc/needrestart/needrestart.conf
+
+
 # Actualiza el sistema y los paquetes
 sudo apt update && sudo apt upgrade -y
+
+# Configura dpkg para instalaciones desatendidas
+echo "force-unsafe-io" | sudo tee /etc/dpkg/dpkg.cfg.d/01_nodoc
 
 # Instala Ubuntu Desktop (ejemplo: Ubuntu Desktop)
 # sudo apt install ubuntu-desktop-minimal
 # for pure and minimal installation of gnome
 sudo apt install gnome-session -y
+
+sudo apt-get install gnome-tweak-tool
+
+# Actualiza la lista de paquetes
+sudo apt update
+
+# Instala el tema del cursor Oxygen
+sudo apt install oxygen-cursor-theme -y
+
+# Configura el tema del cursor Oxygen como el tema predeterminado
+sudo update-alternatives --config x-cursor-theme
 
 # Install auto-cpufreq for laptops
 if ! command -v auto-cpufreq &>/dev/null; then
@@ -16,7 +37,7 @@ fi
 
 # Configure auto-cpufreq
 if ! [ -f /etc/auto-cpufreq.conf ]; then
-    sudo tee /etc/auto-cpufreq.conf > /dev/null <<EOL
+    sudo tee /etc/auto-cpufreq.conf >/dev/null <<EOL
 # Configuration for when the laptop is using battery
 [battery]
 governor = powersave
@@ -51,6 +72,16 @@ done
 setxkbmap us -variant colemak
 
 # Install Alacritty and other useful tools
+
+# Install necesary packages to install alacrity
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+
+# Install rust for alacrity
+sudo curl https://sh.rustup.rs -sSf | sh
+
+# Add repo for install alacrity
+sudo add-apt-repository ppa:aslatter/ppa -y
+
 sudo apt install alacritty git curl wget build-essential -y
 
 # Configure Alacritty as the default terminal
@@ -64,6 +95,9 @@ sudo apt install python3-pip -y
 
 # Install Flatpak
 sudo apt install flatpak -y
+
+# add flatpak repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install applications via Flatpak
 flatpak_apps=(
@@ -97,16 +131,35 @@ done
 sudo apt-get install libgtk2.0-0 libasound2 libdbus-glib-1-2 -y
 
 # Install Docker (optional)
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 if ! command -v docker &>/dev/null; then
     # Set up Docker's Apt repository
-    sudo apt-get install ca-certificates curl gnupg -y
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
+    sudo systemctl enable docker.service
+    sudo systemctl enable containerd.service
+
+    # #To stop this behavior, use disable instead.
+    # sudo systemctl disable docker.service
+    # sudo systemctl disable containerd.service
 fi
 
 # Install Node.js and npm
@@ -133,7 +186,7 @@ fi
 if ! command -v go &>/dev/null; then
     curl -OL https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
     sudo tar -C /usr/local -xvf go1.21.1.linux-amd64.tar.gz
-    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a ~/.profile > /dev/null
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a ~/.profile >/dev/null
     source ~/.profile
 fi
 
@@ -149,7 +202,7 @@ chmod +x ResponsivelyApp-1.8.0.AppImage
 app_dir="/usr/local/share/applications/ResponsivelyApp"
 sudo mkdir -p "$app_dir"
 sudo mv ResponsivelyApp-1.8.0.AppImage "$app_dir/"
-sudo tee "$app_dir/ResponsivelyApp.desktop" > /dev/null <<EOL
+sudo tee "$app_dir/ResponsivelyApp.desktop" >/dev/null <<EOL
 [Desktop Entry]
 Name=ResponsivelyApp
 Exec="$app_dir/ResponsivelyApp-1.8.0.AppImage"
