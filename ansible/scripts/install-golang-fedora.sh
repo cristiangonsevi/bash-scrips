61d218a9d05dc86559a810e3a9ca75138e90e076
Cristian Gonzalez <albertsevilla1996@gmail.com>
2023-10-11 22:00:49 -0600

    fix: :bug: fix bug

---
+++ ansible/scripts/install-golang-fedora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v go &>/dev/null; then
+    curl -OL https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xvf go1.21.1.linux-amd64.tar.gz
+    echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a ~/.profile >/dev/null
+    source ~/.profile
+fi
